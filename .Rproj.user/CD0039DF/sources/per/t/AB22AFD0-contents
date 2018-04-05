b7eb0499cefabb12783689d491a8ea6bdd487460
Silius Mortensønn Vandeskog <siliusv@gmail.com>
2018-04-05 12:45:20 +0200

    first commit

---
+++ .Rproj.user/CD0039DF/sources/per/t/AB22AFD0-contents
@@ -0,0 +1,73 @@
+
+# Euclidian distance function
+eucDist <- function(all_points, point){
+  
+  res <- sweep(all_points, 2, point, "-")
+  res <- res^2
+  res <- apply(res, 1, sum)
+  res <- sqrt(res)
+  
+  return(res)
+}
+
+
+# Find k nearest neighbours
+findKnn <- function(all_points, point, distFunc, k, values){
+  
+  distances <- distFunc(all_points, point)
+  
+  ord <- order(distances)
+  
+  res <- list(distances = distances[1:k],
+              values = values[ord][1:k])
+  
+  return(res)
+  
+}
+
+
+# Classification by voting
+vote <- function(closest){
+  
+  t <- table(closest$values)
+  
+  or <- order(t, decreasing = TRUE)
+  
+  if(t[or[1]] == t[or[2]]){
+    # return("You need to fix voting when two or more classes 
+           # are equally well represented")
+    equal_votes <- t[which(t == t[or[1]])]
+    
+    for(i in 1:k){
+      p <- which(names(equal_votes) == as.character(closest$values[i])) 
+      if(p){
+        return(names(equal_votes)[p])
+      }
+    }
+  }
+  
+  return(names(t)[or[1]])
+}
+
+
+knn <- function(k, data, point, type){
+  
+  all_points <- as.matrix(data[, -dim(data)[2]])
+  values <- data[, dim(data)[2]]
+  
+  closest <- findKnn(all_points = all_points,
+                     point = point,
+                     k = k,
+                     values = values,
+                     distFunc = eucDist)
+  
+  if(type == "reg"){
+    return(mean(closest$values))
+  } else if(type == "class"){
+    return(vote(closest))
+  }
+  
+  return("This type is not accepted")
+}
+
+
---
+++ stupidname.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 31 21:33:39 2018
+
+@author: siliusmv
+"""
+import numpy as np
+from sklearn import tree
+
+
+
+
+#knn_class = np.genfromtxt("dataset/dataset/knn_classification.csv", delimiter=",", skip_header=1)
+#knn_reg = np.genfromtxt("dataset/dataset/knn_regression.csv", delimiter=",", skip_header=1)
+#ada_test = np.genfromtxt("dataset/dataset/adaboost_test.csv", delimiter=",", skip_header=1)
+#ada_train = np.genfromtxt("dataset/dataset/adaboost_train.csv", delimiter=",", skip_header=1)
+
+
+#lines = inspect.getsourcelines(test)    
+#print("".join(lines[0]))
+
+# Returns classifier for some data
+# using T iterations of adaBoost
+def adaBoost(data, T):
+    dim = data.shape
+    
+    x = data[0:dim[0], 2:(dim[1]-1)]     
+    y = data[0:dim[0], 1]
+
+    classifiers = []
+    all_as = []
+    
+    weight = np.ones(dim[0]) * (1. / dim[0])
+
+    for i in range(T):
+            
+        clf = tree.DecisionTreeClassifier(max_depth = 1)
+        clf = clf.fit(x, y, sample_weight = weight)
+        pred = clf.predict(x)
+        
+        epsilon = sum((pred != y) * weight)
+        a = (1. / 2) * np.log((1 - epsilon) / epsilon)
+        
+        nw = weight * np.e**(-a * y * pred)
+        nw = nw / sum(nw)
+        weight = nw
+        
+        classifiers.append(clf)
+        all_as.append(a)
+        
+    return(list([classifiers, all_as]))
+        
+# Returns predictions from an
+# adaBoost algorithm
+def adaPred(points, classifier):
+    
+    n = len(classifier[0])
+    
+    s = np.zeros(points.shape[0])
+        
+    for i in range(n):
+        s = s + classifier[0][i].predict(points) * classifier[1][i]
+    
+    return(np.sign(s))
+    
+# Misclassification error    
+def misClass(pred, y):
+    return(sum(pred != y) / float(len(y)))
+
+
+# Return an error vector for iteration 1 through max_iter
+def testAdaBoost(max_iter, train_data, test_data):
+    
+    test_dim = test_data.shape
+    x_test = test_data[0:test_dim[0], 2:(test_dim[1]-1)]
+    y_test = test_data[0:test_dim[0], 1]
+    errors = []
+    
+    for i in range(max_iter):
+        classifier = adaBoost(train_data, i+1)
+        pred = adaPred(x_test, classifier)
+        errors.append(misClass(pred, y_test))
+        
+    return(errors)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
